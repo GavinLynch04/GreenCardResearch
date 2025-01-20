f38a42b44c294379fc44c1099aa4b7a205e7569f
Gavin Lynch <gavinlynch04@gmail.com>
2025-01-19 22:07:07 -0800

    added a catboost model

--- Models/CatBoost.py
+++ Models/CatBoost.py
@@ -5,12 +5,14 @@ from catboost import CatBoostClassifier
 
 X, y, cat = preprocess()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=27)
+class_weights = [0.84, 2.88, 1.26, 0.60]
 
 model = CatBoostClassifier(
     iterations=500,          # Number of boosting iterations
     learning_rate=0.1,       # Learning rate
     depth=12,                 # Depth of trees
     cat_features=cat,  # Specify categorical features
+    class_weight=class_weights,
     verbose=1               # Print progress every 1 iterations
 )
 
